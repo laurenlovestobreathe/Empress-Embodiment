91d9e7f683374208aadd42c590df721cc67ad4f6
laurenlovestobreathe <laurenwright@thenaturalnipple.com>
2026-03-15 06:17:43 +0400

    Initialize React application with EmpressEmbodimentBot

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EmpressEmbodimentBot from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <EmpressEmbodimentBot />
+  </React.StrictMode>
+);
